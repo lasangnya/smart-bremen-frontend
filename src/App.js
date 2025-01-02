ab996ec386b08d522fd60d472ca77f891307712f
lasan <lasan@uni-bremen.de>
2025-01-03 00:07:25 +0530

    designed custom button

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import Button from './components/Button';
+import './styles/colors.css';
+import './App.css';
 
 const App = () => {
   return (
     <div>
       <Header />
+      <div style={{textAlign : 'center', margin: '20px 0', fontFamily: 'CircularStd', fontWeight:'700', color: 'var(--secondary)', fontSize: '25px'}}>
+        Like to get featured?
+      </div>
+      <div className='button-container'>
+        <Button text="CONTACT US" onClick={() => {}} />
+      </div>
       <Footer />
     </div>
   );
