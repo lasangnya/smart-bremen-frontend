1a196defc39038919eba6bb8d9758f113b222037
lasan <lasan@uni-bremen.de>
2025-01-06 13:30:19 +0530

    completed contact us form

--- src/pages/ContactUs.js
+++ src/pages/ContactUs.js
@@ -11,9 +11,27 @@ const ContactUs = () => {
     <div className='contactus-container'>
       <Header />
       <div className='contactus-content'>
-      <div style={{textAlign : 'center', margin: '20px 0', fontFamily: 'CircularStd', fontWeight:'700', color: 'var(--secondary)', fontSize: '25px'}}>
-        Contact Us Page
-      </div>
+        <form className='contactus-form'>
+          <div className='form-group'>
+            <label htmlFor='name'>Name</label>
+            <input type='text' id='name' name='name' required minLength='3' maxLength='50'/>
+          </div>
+          <div className='form-group'>
+            <label htmlFor='email'>Email address</label>
+            <input type='email' id='email' name='email' required minLength='5' maxLength='50'/>
+          </div>
+          <div className='form-group'>
+            <label htmlFor='name'>Subject</label>
+            <input type='text' id='subject' name='subject' required minLength='3' maxLength='100'/>
+          </div>
+          <div className='form-group'>
+            <label htmlFor='name'>Message</label>
+            <textarea id='message' name='message' rows = '4' required minLength='10' maxLength='1000'/>
+          </div>
+          <div className='send-message-button-container'>
+            <button type='submit' className='send-message-button'>SEND MESSAGE</button>
+          </div>
+        </form>
       </div>      
       <Footer />
     </div>
